f9f338e78ea3e6bfa3f76398d2daeacf5e2eba50
tcckn <01verum.gard@gmail.com>
2018-09-21 13:46:47 +0900

    first commit

---
+++ src/main/java/com/dev_training/dev_training/DevTrainingApplication.java
@@ -0,0 +1,12 @@
+package com.dev_training.dev_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevTrainingApplication.class, args);
+    }
+}
