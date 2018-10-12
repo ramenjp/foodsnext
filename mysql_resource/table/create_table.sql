d936a16b1b74cbe3e8e0f718379baf528c2d01ea
thikethike <t.looklook.sato.0921@gmail.com>
2018-10-12 17:58:22 +0900

    TODOのDDLを追加

--- mysql_resource/table/create_table.sql
+++ mysql_resource/table/create_table.sql
@@ -13,3 +13,19 @@ CREATE TABLE `accounts` (
   `updated_tms` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `todo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  `detail` text NOT NULL,
+  `remarks` text,
+  `start_date` char(10) DEFAULT NULL,
+  `end_date` char(10) DEFAULT NULL,
+  `issue_person_id` int(11) NOT NULL,
+  `person_in_charge_id` int(11) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `priority` varchar(20) NOT NULL,
+  `created_tms` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_tms` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
