5ba97c6e377ff259af7d1a24e58b5655191d8022
thikethike <t.looklook.sato.0921@gmail.com>
2018-10-23 13:45:05 +0900

    ログイン画面をかっこよくしてみる

--- src/main/java/com/dev_training/controller/LoginController.java
+++ src/main/java/com/dev_training/controller/LoginController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 
     @RequestMapping(value="")
-    public String loginForm() {
+    public String login() {
         return "login/loginForm";
     }
 }
