1858d5ea8c70e01b5c7ab3e7383dd0179321a7a1
thikethike <t.looklook.sato.0921@gmail.com>
2018-12-12 14:53:18 +0900

    パスワード変更機能実装 トップ画面にGravator連動機能実装 アカウント更新確認画面の不要なクラスアペンドを削除

--- src/main/java/com/dev_training/controller/TopController.java
+++ src/main/java/com/dev_training/controller/TopController.java
@@ -1,5 +1,6 @@
 package com.dev_training.controller;
 
+import com.dev_training.common.MD5Util;
 import com.dev_training.entity.Account;
 import com.dev_training.service.TopService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,7 @@ public class TopController {
     private final HttpSession session;
     /** セッションキー(ログインユーザのアカウント) */
     private static final String SESSION_FORM_ID = "account";
+    private static final String GRAVATAR_URL_PREFIX = "http://www.gravatar.com/avatar/";
 
     @Autowired
     public TopController(TopService topService, HttpSession session) {
@@ -43,6 +45,7 @@ public class TopController {
         if (Objects.isNull(session.getAttribute(SESSION_FORM_ID))) {
             Account sessionAccount = service.getAccountById(account.getId());
             session.setAttribute(SESSION_FORM_ID, sessionAccount);
+            session.setAttribute("gravatorURL", GRAVATAR_URL_PREFIX + MD5Util.md5Hex(sessionAccount.getEmail()));
         }
         return "top/topForm";
     }
