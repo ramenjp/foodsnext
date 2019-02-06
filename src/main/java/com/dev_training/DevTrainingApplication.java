22b277f1207f59dcaa87f4f967573e19c3420b31
shotgw <shosuke.tsugawa@accenture.com>
2019-02-06 11:03:30 +0900

    warを作成できるように変更

--- src/main/java/com/dev_training/DevTrainingApplication.java
+++ src/main/java/com/dev_training/DevTrainingApplication.java
@@ -2,13 +2,21 @@ package com.dev_training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * アプリケーションのメインクラス。
  */
 @SpringBootApplication
-public class DevTrainingApplication {
+public class DevTrainingApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(DevTrainingApplication.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DevTrainingApplication.class);
+    }
 }
+
