c132a600f352a929eff73cecf112d07b265cc6da
toshihiro.satomi <t.looklook.sato.0921@gmail.com>
2019-03-05 10:01:29 +0900

    #24 コメント機能実装 改行文字をBR変換するよう修正

--- src/main/resources/static/js/training.js
+++ src/main/resources/static/js/training.js
@@ -8,3 +8,15 @@ function removeTags(sentence){
     sentence = sentence.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'');
     return sentence;
 }
+
+/**
+ * 改行コードを改行タグに変換する。
+ *
+ * @param sentence 対象の文字列
+ * @returns {string} 変換後の文字列
+ */
+function convertBr(sentence){
+    sentence = sentence.replace(/\r?\n/g, '<br>');
+    return sentence;
+}
+
