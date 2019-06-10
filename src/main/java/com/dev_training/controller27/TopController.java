ef406048e32d10b3333493e185cf8f876787bb9b
tsukimi.ono <tsukimi.ono@rakuten.com>
2019-06-10 14:19:30 +0900

    b

--- src/main/java/com/dev_training/controller27/TopController.java
+++ src/main/java/com/dev_training/controller27/TopController.java
@@ -1,7 +1,9 @@
 package com.dev_training.controller27;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * トップコントローラー
@@ -9,6 +11,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/top")
 public class TopController {
-    public String match(){return"/top/matching1";}
+
+    /**
+     * トップ画面
+     * @param
+     * @return
+     */
+    @RequestMapping(value = "/matching1", params = "match", method = RequestMethod.POST)
+    public String match(){return"matching_count";}
+
+    @RequestMapping(value = "/history", params = "historyCheck", method = RequestMethod.POST)
+    public String historyCheck(){ return "historyCheck";}
+
+    @RequestMapping(value = "/setting", params = "accountUpdate", method = RequestMethod.POST)
+    public String accountUpdate(){ return "accountUpdate";}
 
 }
--- src/main/java/com/dev_training/service27/AccountRegisterService.java
+++ src/main/java/com/dev_training/service27/AccountRegisterService.java
@@ -1,4 +1,31 @@
 package com.dev_training.service27;
 
+import com.dev_training.entity27.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * アカウント登録サービス
+ */
+@Service
 public class AccountRegisterService {
+
+    /** アカウントレポジトリ　*/
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountRegisterService(AccountRepository accountRepository){this.accountRepository = accountRepository;}
+
+    /**
+     * 登録処理
+     *
+     * @param form フォーム
+     */
+
+    @Transactional
+    public void register(AccountRegisterForm form){
+        Account account = new Account();
+        accountRepository.save(account);
+    }
 }
